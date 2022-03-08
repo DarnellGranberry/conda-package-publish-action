6418993ff35475439397fbd5ddb0206c084c406c
Darnell Granberry <40174000+DarnellGranberry@users.noreply.github.com>
2022-03-08 17:11:52 -0500

    Removed OS conversion and changed upload path

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,13 +18,15 @@ check_if_meta_yaml_file_exists() {
 
 build_package(){
     conda build -c conda-forge -c bioconda --output-folder . .
-    conda convert -p osx-64 linux-64/*.tar.bz2
+    #conda convert -p osx-64 linux-64/*.tar.bz2
 }
 
 upload_package(){
     export ANACONDA_API_TOKEN=$INPUT_ANACONDATOKEN
-    anaconda upload --label main linux-64/*.tar.bz2
-    anaconda upload --label main osx-64/*.tar.bz2
+    anaconda upload --label main ./*.tar.bz2
+    #anaconda upload --label main ./noarch/topaz-0.2.5a-py_0.tar.bz2 #original line
+    #anaconda upload --label main linux-64/*.tar.bz2
+    #anaconda upload --label main osx-64/*.tar.bz2
 }
 
 go_to_build_dir
